d5466a75ac49af3b80e7af9c52a287b00960164a
Aldemitar <aldemarcatanocardona@gmail.com>
2022-07-22 11:55:37 -0500

    Primer proyecto de php

---
+++ app/Models/Country.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    //la tabla con la cual el modelo se relaciona
+    protected $table = "countries";
+    //La clave primara de la tabla
+    protected $primaryKey = "country_id";
+    //Anular campos de auditoria
+    public $timestamps = false;
+    use HasFactory;
+}
